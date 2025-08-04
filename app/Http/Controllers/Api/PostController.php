a753eb8ccde4389d89e0ea4f8ec7cc680eb428d8
mannanshihab <amannanshihab@gmail.com>
2025-08-04 19:42:13 +0600

    add:create-post-api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -18,4 +19,39 @@ class PostController extends Controller
             'posts' => $posts
         ], 200);
     }
+
+    public function store(Request $request){
+        $validateUser = Validator::make(
+            $request->all(),
+            [
+                'title' => 'required',
+                'description' => 'required',
+                'image' => 'required'
+            ]
+        );
+        if($validateUser->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'errors' => $validateUser->errors()->all()
+            ], 401);
+        }
+
+        $img = $request->image;
+        $ext = $img->getClientOriginalExtension();
+        $imageName = time().'.'.$ext;
+        $img->move(public_path('images'), $imageName);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $imageName,
+        ]);
+        return response()->json([
+            'status' => true,
+            'message' => 'Post Created Successfully',
+            'post' => $post
+        ], 200);
+    }
+    
 }
